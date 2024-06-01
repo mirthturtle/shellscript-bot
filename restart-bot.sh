5250e6158650f49a91ddac57d14b19609bab67ec
Christian DeWolf <me@christiandewolf.com>
2024-06-01 18:06:48 -0300

    Add a start bot script

--- restart-bot.sh
+++ restart-bot.sh
@@ -2,10 +2,7 @@
 
 # Restarts the discord bot
 
-# Define the name of your Node.js application file
 APP_NAME="bot.js"
-
-# Find the process ID (PID) of the running Node.js application
 PID=$(pgrep -f "node $APP_NAME")
 
 # Check if the process is running and kill it
@@ -17,8 +14,5 @@ else
   echo "No running Node.js application found."
 fi
 
-# Start the Node.js application with nohup
-echo "Starting Node.js application..."
 nohup node $APP_NAME &
-
-echo "Node.js application started."
+echo "bot.js started."
---
+++ start-bot.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Starts the discord bot.
+
+APP_NAME="bot.js"
+
+nohup node $APP_NAME &
+echo "bot.js started."
